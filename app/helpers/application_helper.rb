5c5c96e7e6d745e592d69f6a3d994ed957ab912a
Mitchell Barron <mitchell.stocky@gmail.com>
2015-06-22 22:05:00 -0500

    Add AC's Top Ten restaurants as suggestions to homepage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def get_path(restaurant_name)
+    r = Restaurant.search(restaurant_name).first
+    if r
+      restaurants_path(r)
+    end
+  end
+
+  def get_violation_count(restaurant_name)
+    if Restaurant.search(restaurant_name).exists?
+      result = Restaurant.search(restaurant_name).first.violations.count
+    else
+      0
+    end
+  end
+
 end
